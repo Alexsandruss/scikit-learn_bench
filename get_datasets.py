079703a29a88e1223624109f499ae06d5d9d7fb7
Alexander Andreev <alexander.andreev@intel.com>
2022-03-11 19:46:21 +0300

    Add working script for datasets downloading

---
+++ get_datasets.py
@@ -0,0 +1,27 @@
+from datasets.load_datasets import dataset_loaders
+import numpy as np
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--datasets', type=str, nargs='*',
+                    help='The datasets that should be downloaded.')
+args = parser.parse_args()
+
+if args.datasets is None:
+    args.datasets = list(dataset_loaders.keys())
+
+print('Datasets for downloading:', args.datasets, sep='\n')
+
+datasets_dir = 'data'
+for dataset in args.datasets:
+    dataset_loaders[dataset](datasets_dir)
+    for subset in ['train', 'test', 'valid']:
+        try:
+            x = np.load(f'{datasets_dir}/{dataset}_x_{subset}.npy', allow_pickle=True)
+            y = np.load(f'{datasets_dir}/{dataset}_y_{subset}.npy', allow_pickle=True)
+            data = pd.DataFrame(x, columns=[f'f{i}' for i in range(x.shape[1])])
+            data['target'] = y
+            data.to_csv(f'{datasets_dir}/{dataset}_{subset}.csv', index=False)
+        except:
+            pass
